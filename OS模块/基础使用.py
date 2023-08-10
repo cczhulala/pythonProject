a257175613f8efe5c1f16f947b4043d837e8aab5
ccz <changzhichen@outlook.com>
2023-08-10 14:22:04 +0800

    测试

---
+++ OS模块/基础使用.py
@@ -0,0 +1,9 @@
+# Developer: ccz
+# Time: 2023/8/10 9:55
+import os
+#os.system('notepad.exe')
+#os.system('calc.exe')
+#直接调用可执行文件
+#os.startfile("C:\\Program Files (x86)\\Tencent\\WeChat\\WeChat.exe")
+print(os.getcwd())
+print(os.listdir(os.getcwd()))
